ab56f808fbd2ac449b5577daf44f93d03fcf6d53
Kenneth Nilsson-Snow <51213931+knilssonsnow@users.noreply.github.com>
2023-03-19 17:25:49 -0400

    added code for length requirements as well as prompt for what to include in the generated password.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Password_Generator
+
+# Password Generator
+
+A generator that uses a combinations of Alphebetical, Numerical, and Special Characters to generate a password that is atleast 8 characters long and no longer than 128 characters.
+
+
+## Authors
+
+- [@knilssonsnow](https://www.github.com/knilssonsnow)
+
+
+## Installation
+
+Just run the website, No Installation required.
--- script.js
+++ script.js
@@ -25,7 +25,9 @@ var specialCharacters = [
 	".",
 ];
 
-var lowerCaseCharacters = [
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var lowerCasedCharacters = [
 	"a",
 	"b",
 	"c",
@@ -54,7 +56,7 @@ var lowerCaseCharacters = [
 	"z",
 ];
 
-var upperCaseCharacters = [
+var upperCasedCharacters = [
 	"A",
 	"B",
 	"C",
@@ -83,7 +85,55 @@ var upperCaseCharacters = [
 	"Z",
 ];
 
-var numericalCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//* fuction for password options prompt as well as password length being minimum of 8 and maximum of 128.
+
+function getPasswordOptions() {
+	var length = parseInt(prompt("How many characters would you like your password to contain?"), 10);
+
+	if (Number.isNaN(length)) {
+		alert("Password length must be provided as a number");
+		return null;
+	}
+
+	if (length < 8) {
+		alert("Password length must be at least 8 characters");
+		return null;
+	}
+
+	if (length > 128) {
+		alert("Password length must less than 129 characters");
+		return null;
+	}
+
+	var hasSpecialCharacters = confirm("Include Special Characters?");
+
+	var hasNumericCharacters = confirm("Include Numerical Characters?");
+
+	var hasLowerCasedCharacters = confirm("Include Lowercase Characters?");
+
+	var hasUpperCasedCharacters = confirm("Include Uppercase Characters?");
+
+	if (
+		hasSpecialCharacters === false &&
+		hasNumericCharacters === false &&
+		hasLowerCasedCharacters === false &&
+		hasUpperCasedCharacters === false
+	) {
+		alert("Must select at least one character type");
+		return null;
+	}
+
+	var passwordOptions = {
+		length: length,
+		hasSpecialCharacters: hasSpecialCharacters,
+		hasNumericCharacters: hasNumericCharacters,
+		hasLowerCasedCharacters: hasLowerCasedCharacters,
+		hasUpperCasedCharacters: hasUpperCasedCharacters,
+	};
+
+	return passwordOptions;
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
